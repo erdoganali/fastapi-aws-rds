30dc6a910776c6775a3c5c0163b796cd06df85a0
Ali Erdoğan <alierdogan@live.com>
2023-06-08 19:32:41 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -53,23 +53,24 @@ def load_model_from_s3():
 # def create_db_and_tables():
 #     SQLModel.metadata.create_all(engine) 
  
-DATABASE_USER = os.environ['DATABASE_USER']
-DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-DATABASE_HOST = os.environ['DATABASE_HOST']
-DATABASE_PORT = os.environ['DATABASE_PORT']
-DATABASE_NAME = os.environ['DATABASE_NAME']
 
-DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:3306/{DATABASE_NAME}"
-
-engine = create_engine(DATABASE_URL)
   
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# def create_db_and_tables():
+#     DATABASE_USER = os.environ['DATABASE_USER']
+#     DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+#     DATABASE_HOST = os.environ['DATABASE_HOST']
+#     DATABASE_PORT = os.environ['DATABASE_PORT']
+#     DATABASE_NAME = os.environ['DATABASE_NAME']
 
-def get_db():
-    db = Session(engine)
-    try:
-        yield db
-    finally:
-        db.close()
+#     DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:3306/{DATABASE_NAME}"
+
+#     engine = create_engine(DATABASE_URL)
+#     SQLModel.metadata.create_all(engine)
+
+# def get_db():
+#     db = Session(engine)
+#     try:
+#         yield db
+#     finally:
+#         db.close()
  
